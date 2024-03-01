6bf67497fd6dc12f8c498f838c73a55135f3a69b
Aurélien Mino <aurelien.mino@gmail.com>
2024-03-02 00:00:31 +0100

    Support Jakarta JTA @Transactional annotation

---
+++ src/test/java/org/jqassistant/plugin/spring/test/set/transaction/JtaJakartaTransactionalClass.java
@@ -0,0 +1,11 @@
+package org.jqassistant.plugin.spring.test.set.transaction;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+public class JtaJakartaTransactionalClass {
+
+    public void transactionalMethod(){
+    }
+
+}
