2a5585a56a881d763dd4b2a1a5654ba5500697f7
DirkMahler <DirkMahler@users.noreply.github.com>
2024-08-01 15:55:36 +0000

    deploy: 60866bc87ca4bc467a4f3ddf79a3115ba58de638

--- snapshot/src/test/java/org/jqassistant/plugin/spring/test/concept/TransactionalMethodIT.java
+++ snapshot/src/test/java/org/jqassistant/plugin/spring/test/concept/TransactionalMethodIT.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.methodDescriptor;
-import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -15,44 +14,28 @@ class TransactionalMethodIT extends AbstractSpringIT {
 
     @Test
     void transactionalMethod() throws Exception {
-        scanClasses(SpringTransactionalMethod.class, JtaTransactionalMethod.class, JtaJakartaTransactionalMethod.class);
+        scanClasses(SpringTransactionalMethod.class, JtaTransactionalMethod.class, JtaJakartaTransactionalMethod.class,
+            SpringTransactionalClass.class, SpringTransactionalSubClass.class, SpringTransactionalInterface.class,
+            SpringTransactionalImplementingClass.class, JtaTransactionalClass.class,
+            JtaJakartaTransactionalClass.class);
         assertThat(applyConcept("spring-transaction:TransactionalMethod").getStatus(), equalTo(SUCCESS));
         store.beginTransaction();
         List<Object> methods = query("MATCH (m:Spring:Method:Transactional) RETURN m").getColumn("m");
-        assertThat(methods.size(), equalTo(3));
+        assertThat(methods.size(), equalTo(10));
+        // method level annotations
         assertThat(methods, hasItem(methodDescriptor(SpringTransactionalMethod.class, "transactionalMethod")));
         assertThat(methods, not(hasItem(methodDescriptor(SpringTransactionalMethod.class, "nonTransactionalMethod"))));
         assertThat(methods, hasItem(methodDescriptor(JtaTransactionalMethod.class, "transactionalMethod")));
         assertThat(methods, hasItem(methodDescriptor(JtaJakartaTransactionalMethod.class, "transactionalMethod")));
-        store.commitTransaction();
-    }
-
-    @Test
-    void transactionalClass() throws Exception {
-        scanClasses(SpringTransactionalClass.class, SpringTransactionalSubClass.class,
-            SpringTransactionalInterface.class, SpringTransactionalImplementingClass.class,
-            JtaTransactionalClass.class, JtaJakartaTransactionalClass.class);
-        assertThat(applyConcept("spring-transaction:TransactionalClass").getStatus(), equalTo(SUCCESS));
-        store.beginTransaction();
-        List<Object> classes = query("MATCH (c:Spring:Type:Transactional) RETURN c").getColumn("c");
-        assertThat(classes.size(), equalTo(6));
-        assertThat(classes, hasItem(typeDescriptor(SpringTransactionalClass.class)));
-        assertThat(classes, hasItem(typeDescriptor(SpringTransactionalSubClass.class)));
-        assertThat(classes, hasItem(typeDescriptor(SpringTransactionalInterface.class)));
-        assertThat(classes,
-            hasItem(typeDescriptor(SpringTransactionalImplementingClass.class)));
-        assertThat(classes, hasItem(typeDescriptor(JtaTransactionalClass.class)));
-        assertThat(classes, hasItem(typeDescriptor(JtaJakartaTransactionalClass.class)));
-        List<Object> methods = query("MATCH (m:Spring:Method:Transactional) RETURN m").getColumn("m");
-        assertThat(methods.size(), equalTo(7));
+        // class level annotations
         assertThat(methods, hasItem(methodDescriptor(SpringTransactionalClass.class, "transactionalMethod")));
         assertThat(methods,
             hasItem(methodDescriptor(SpringTransactionalSubClass.class, "transactionalSubClassMethod")));
         assertThat(methods, hasItem(methodDescriptor(SpringTransactionalInterface.class, "transactionalMethod")));
         assertThat(methods,
             hasItem(methodDescriptor(SpringTransactionalImplementingClass.class, "transactionalMethod")));
-        assertThat(methods,
-            hasItem(methodDescriptor(SpringTransactionalImplementingClass.class, "transactionalImplementingClassMethod")));
+        assertThat(methods, hasItem(
+            methodDescriptor(SpringTransactionalImplementingClass.class, "transactionalImplementingClassMethod")));
         assertThat(methods, hasItem(methodDescriptor(JtaTransactionalClass.class, "transactionalMethod")));
         assertThat(methods, hasItem(methodDescriptor(JtaJakartaTransactionalClass.class, "transactionalMethod")));
         store.commitTransaction();
