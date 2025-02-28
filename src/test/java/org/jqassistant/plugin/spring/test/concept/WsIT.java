ed01429f023bd08a895ae06d83a9cbdeecb2b623
Candy Lohse <candy.lohse@buschmais.com>
2025-02-28 15:07:55 +0100

    #27 Use AssertJ instead of Hamcrest

--- src/test/java/org/jqassistant/plugin/spring/test/concept/WsIT.java
+++ src/test/java/org/jqassistant/plugin/spring/test/concept/WsIT.java
@@ -10,9 +10,7 @@ import com.buschmais.jqassistant.core.rule.api.model.Concept;
 import com.buschmais.jqassistant.core.rule.api.model.RuleException;
 import com.buschmais.jqassistant.plugin.java.api.model.MethodDescriptor;
 import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
-import com.buschmais.jqassistant.plugin.java.test.assertj.TypeDescriptorCondition;
 
-import org.assertj.core.api.Assertions;
 import org.jqassistant.plugin.spring.test.set.components.Endpoint;
 import org.jqassistant.plugin.spring.test.set.components.RestController;
 import org.junit.jupiter.api.Test;
@@ -21,11 +19,9 @@ import org.springframework.ws.test.client.MockWebServiceServer;
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.WARNING;
 import static com.buschmais.jqassistant.plugin.java.test.assertj.MethodDescriptorCondition.methodDescriptor;
-import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
+import static com.buschmais.jqassistant.plugin.java.test.assertj.TypeDescriptorCondition.typeDescriptor;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.type;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class WsIT extends AbstractSpringIT {
     @Test
@@ -36,13 +32,13 @@ public class WsIT extends AbstractSpringIT {
 
     private void verify(Class<?> componentType, String conceptId, String expectedLabels) throws RuleException {
         scanClasses(RestController.class);
-        assertThat(applyConcept(conceptId).getStatus(), equalTo(WARNING));
+        assertThat(applyConcept(conceptId).getStatus()).isEqualTo(WARNING);
         clearConcepts();
         scanClasses(componentType);
-        assertThat(applyConcept(conceptId).getStatus(), equalTo(SUCCESS));
+        assertThat(applyConcept(conceptId).getStatus()).isEqualTo(SUCCESS);
         store.beginTransaction();
-        assertThat(query("MATCH (c" + expectedLabels + ") RETURN c").getColumn("c"),
-            hasItem(typeDescriptor(componentType)));
+        final List<TypeDescriptor> types = query("MATCH (c" + expectedLabels + ") RETURN c").getColumn("c");
+        assertThat(types).haveExactly(1, typeDescriptor(componentType));
         store.commitTransaction();
     }
 
@@ -51,12 +47,12 @@ public class WsIT extends AbstractSpringIT {
         scanClasses(AssertExample.class, AssertExample.ExampleResultActions.class);
 
         final Result<Concept> conceptResult = applyConcept("spring-ws:MockWebServiceServerVerifyMethod");
-        Assertions.assertThat(conceptResult.getStatus()).isEqualTo(SUCCESS);
+        assertThat(conceptResult.getStatus()).isEqualTo(SUCCESS);
 
         store.beginTransaction();
 
-        Assertions.assertThat(conceptResult.getRows().size()).isEqualTo(1);
-        Assertions.assertThat(conceptResult.getRows()
+        assertThat(conceptResult.getRows().size()).isEqualTo(1);
+        assertThat(conceptResult.getRows()
                 .get(0)
                 .getColumns()
                 .get("assertMethod")
@@ -73,7 +69,7 @@ public class WsIT extends AbstractSpringIT {
         scanClasses(AssertExample.class, AssertExample.ExampleResultActions.class);
 
         final Result<Concept> conceptResult = applyConcept("java:AssertMethod");
-        Assertions.assertThat(conceptResult.getStatus()).isEqualTo(SUCCESS);
+        assertThat(conceptResult.getStatus()).isEqualTo(SUCCESS);
 
         store.beginTransaction();
 
@@ -83,8 +79,8 @@ public class WsIT extends AbstractSpringIT {
             .map(Column::getValue)
             .map(TypeDescriptor.class::cast)
             .collect(Collectors.toList());
-        Assertions.assertThat(declaringTypes).haveExactly(1,
-            TypeDescriptorCondition.typeDescriptor(MockWebServiceServer.class));
+        assertThat(declaringTypes).haveExactly(1,
+            typeDescriptor(MockWebServiceServer.class));
 
         verifyAssertMethodResultGraph();
 
@@ -97,10 +93,10 @@ public class WsIT extends AbstractSpringIT {
             "MATCH (testMethod:Method)-[:INVOKES]->(assertMethod:Method) "
                 + "WHERE assertMethod:MockWebServiceServer:Assert "
                 + "RETURN testMethod, assertMethod");
-        Assertions.assertThat(methodQueryResult.getRows().size()).isEqualTo(1);
-        Assertions.assertThat(methodQueryResult.<MethodDescriptor>getColumn("testMethod"))
+        assertThat(methodQueryResult.getRows().size()).isEqualTo(1);
+        assertThat(methodQueryResult.<MethodDescriptor>getColumn("testMethod"))
             .haveExactly(1, methodDescriptor(AssertExample.class, "mockWebServiceServerVerifyExampleMethod"));
-        Assertions.assertThat(methodQueryResult.<MethodDescriptor>getColumn("assertMethod"))
+        assertThat(methodQueryResult.<MethodDescriptor>getColumn("assertMethod"))
             .haveExactly(1, methodDescriptor(MockWebServiceServer.class, "verify"));
     }
 }
