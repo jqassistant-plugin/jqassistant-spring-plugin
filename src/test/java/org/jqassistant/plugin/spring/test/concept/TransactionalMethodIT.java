cfcc3a0d3e0585d22cc0f3c8ce0765062de93d4f
Candy Lohse <candy.lohse@buschmais.com>
2025-03-14 08:33:32 +0100

    #34 Replace Hamcrest with AssertJ

--- src/test/java/org/jqassistant/plugin/spring/test/concept/TransactionalMethodIT.java
+++ src/test/java/org/jqassistant/plugin/spring/test/concept/TransactionalMethodIT.java
@@ -2,13 +2,13 @@ package org.jqassistant.plugin.spring.test.concept;
 
 import java.util.List;
 
+import com.buschmais.jqassistant.plugin.java.api.model.MethodDescriptor;
 import org.jqassistant.plugin.spring.test.set.transaction.*;
 import org.junit.jupiter.api.Test;
 
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
-import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.methodDescriptor;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static com.buschmais.jqassistant.plugin.java.test.assertj.MethodDescriptorCondition.methodDescriptor;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TransactionalMethodIT extends AbstractSpringIT {
 
@@ -18,26 +18,34 @@ class TransactionalMethodIT extends AbstractSpringIT {
             SpringTransactionalClass.class, SpringTransactionalSubClass.class, SpringTransactionalInterface.class,
             SpringTransactionalImplementingClass.class, JtaTransactionalClass.class,
             JtaJakartaTransactionalClass.class);
-        assertThat(applyConcept("spring-transaction:TransactionalMethod").getStatus(), equalTo(SUCCESS));
+        assertThat(applyConcept("spring-transaction:TransactionalMethod").getStatus()).isEqualTo(SUCCESS);
         store.beginTransaction();
-        List<Object> methods = query("MATCH (m:Spring:Method:Transactional) RETURN m").getColumn("m");
-        assertThat(methods.size(), equalTo(10));
+        final List<MethodDescriptor> methods = query("MATCH (m:Spring:Method:Transactional) RETURN m").getColumn("m");
+        assertThat(methods).hasSize(10);
         // method level annotations
-        assertThat(methods, hasItem(methodDescriptor(SpringTransactionalMethod.class, "transactionalMethod")));
-        assertThat(methods, not(hasItem(methodDescriptor(SpringTransactionalMethod.class, "nonTransactionalMethod"))));
-        assertThat(methods, hasItem(methodDescriptor(JtaTransactionalMethod.class, "transactionalMethod")));
-        assertThat(methods, hasItem(methodDescriptor(JtaJakartaTransactionalMethod.class, "transactionalMethod")));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(SpringTransactionalMethod.class, "transactionalMethod"));
+        assertThat(methods).doNotHave(
+            methodDescriptor(SpringTransactionalMethod.class, "nonTransactionalMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(JtaTransactionalMethod.class, "transactionalMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(JtaJakartaTransactionalMethod.class, "transactionalMethod"));
         // class level annotations
-        assertThat(methods, hasItem(methodDescriptor(SpringTransactionalClass.class, "transactionalMethod")));
-        assertThat(methods,
-            hasItem(methodDescriptor(SpringTransactionalSubClass.class, "transactionalSubClassMethod")));
-        assertThat(methods, hasItem(methodDescriptor(SpringTransactionalInterface.class, "transactionalMethod")));
-        assertThat(methods,
-            hasItem(methodDescriptor(SpringTransactionalImplementingClass.class, "transactionalMethod")));
-        assertThat(methods, hasItem(
-            methodDescriptor(SpringTransactionalImplementingClass.class, "transactionalImplementingClassMethod")));
-        assertThat(methods, hasItem(methodDescriptor(JtaTransactionalClass.class, "transactionalMethod")));
-        assertThat(methods, hasItem(methodDescriptor(JtaJakartaTransactionalClass.class, "transactionalMethod")));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(SpringTransactionalClass.class, "transactionalMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(SpringTransactionalSubClass.class, "transactionalSubClassMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(SpringTransactionalInterface.class, "transactionalMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(SpringTransactionalImplementingClass.class, "transactionalMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(SpringTransactionalImplementingClass.class, "transactionalImplementingClassMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(JtaTransactionalClass.class, "transactionalMethod"));
+        assertThat(methods).haveExactly(1,
+            methodDescriptor(JtaJakartaTransactionalClass.class, "transactionalMethod"));
         store.commitTransaction();
     }
 }
--- src/test/java/org/jqassistant/plugin/spring/test/constraint/TransactionalMethodMustNotBeInvokedFromSameClassIT.java
+++ src/test/java/org/jqassistant/plugin/spring/test/constraint/TransactionalMethodMustNotBeInvokedFromSameClassIT.java
@@ -12,24 +12,23 @@ import org.jqassistant.plugin.spring.test.set.transaction.SpringTransactionalMet
 import org.junit.jupiter.api.Test;
 
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.FAILURE;
-import static com.buschmais.jqassistant.core.test.matcher.ConstraintMatcher.constraint;
-import static com.buschmais.jqassistant.core.test.matcher.ResultMatcher.result;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TransactionalMethodMustNotBeInvokedFromSameClassIT extends AbstractJavaPluginIT {
 
     @Test
     void transactionMethodsMustNotBeCalledDirectly() throws Exception {
         scanClasses(SpringTransactionalMethod.class);
-        assertThat(validateConstraint("spring-transaction:TransactionalMethodMustNotBeInvokedFromSameClass").getStatus(), equalTo(FAILURE));
+        assertThat(validateConstraint("spring-transaction:TransactionalMethodMustNotBeInvokedFromSameClass")
+            .getStatus()).isEqualTo(FAILURE);
         store.beginTransaction();
         List<Result<Constraint>> constraintViolations = new ArrayList<>(reportPlugin.getConstraintResults().values());
-        assertThat(constraintViolations.size(), equalTo(1));
+        assertThat(constraintViolations).hasSize(1);
         Result<Constraint> result = constraintViolations.get(0);
-        assertThat(result, result(constraint("spring-transaction:TransactionalMethodMustNotBeInvokedFromSameClass")));
+        assertThat(result.getRule().getId())
+            .isEqualTo("spring-transaction:TransactionalMethodMustNotBeInvokedFromSameClass");
         List<Row> rows = result.getRows();
-        assertThat(rows.size(), equalTo(1));
+        assertThat(rows).hasSize(1);
         store.commitTransaction();
     }
 
