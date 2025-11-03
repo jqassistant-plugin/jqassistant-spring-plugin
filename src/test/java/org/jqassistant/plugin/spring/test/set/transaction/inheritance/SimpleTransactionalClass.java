25da38c6b2376dc5f40f477300c3b5f4729abee9
Candy Lohse <candy.lohse@buschmais.com>
2025-11-03 13:08:31 +0100

    #90 Methods inherited from transactional methods are transactional as well

---
+++ src/test/java/org/jqassistant/plugin/spring/test/set/transaction/inheritance/SimpleTransactionalClass.java
@@ -0,0 +1,12 @@
+package org.jqassistant.plugin.spring.test.set.transaction.inheritance;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class SimpleTransactionalClass {
+
+    public void method() {
+
+    }
+
+}
