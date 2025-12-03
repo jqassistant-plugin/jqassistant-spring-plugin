7b22a1c388ca03b793ca31831f6251b692e86c17
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2025-12-03 14:36:57 +0100

    #98: excluded test artifacts, fixed comments

--- src/test/java/org/jqassistant/plugin/spring/test/set/transaction/NonTransactionalSubClassOfSpringTransactionalMethod.java
+++ src/test/java/org/jqassistant/plugin/spring/test/set/transaction/NonTransactionalSubClassOfSpringTransactionalMethod.java
@@ -2,7 +2,7 @@ package org.jqassistant.plugin.spring.test.set.transaction;
 
 public class NonTransactionalSubClassOfSpringTransactionalMethod extends SpringTransactionalMethod{
 
-    // Method is not actually transactional, since class-level annotations are not inherited.
+    // Method is not actually transactional, since method-level annotations are not inherited.
     @Override
     public void transactionalMethod() {
     }
